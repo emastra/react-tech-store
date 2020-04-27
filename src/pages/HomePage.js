380d7d7bcadfde7a44b95939dfcb629d9f0489d0
emastra <emiliano.mastra@gmail.com>
2020-04-27 15:11:58 +0200

    Context added

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,13 @@
 import React from "react";
-
+import { ProductConsumer } from "../context";
 export default function HomePage() {
   return (
     <>
-      <h1>Hello From Home Page</h1>
+      <ProductConsumer>
+        {value => {
+          return <h1>Hello from HomePage</h1>;
+        }}
+      </ProductConsumer>
     </>
   );
 }
