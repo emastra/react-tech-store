0c71271496b7e4c58dc49ebbc02f45fb1b860ed8
emastra <emiliano.mastra@gmail.com>
2020-05-14 10:24:28 +0200

    reformatting

--- src/context/socialData.js
+++ src/context/socialData.js
@@ -2,19 +2,19 @@ import React from "react";
 import { FaFacebook, FaTwitter, FaLinkedin } from "react-icons/fa";
 
 export const socialData = [
-  {
-    id: 1,
-    icon: <FaFacebook className="icon" />,
-    url: "#"
-  },
-  {
-    id: 2,
-    icon: <FaTwitter className="icon" />,
-    url: "#"
-  },
-  {
-    id: 3,
-    icon: <FaLinkedin className="icon" />,
-    url: "https://www.linkedin.com/in/emiliano-mastragostino/"
-  }
+    {
+        id: 1,
+        icon: <FaFacebook className="icon" />,
+        url: "#"
+    },
+    {
+        id: 2,
+        icon: <FaTwitter className="icon" />,
+        url: "#"
+    },
+    {
+        id: 3,
+        icon: <FaLinkedin className="icon" />,
+        url: "https://www.linkedin.com/in/emiliano-mastragostino/"
+    }
 ];
