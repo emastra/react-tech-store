1e0f89345c95a9f1f958e3aecdd1e94e9786adbe
emastra <emiliano.mastra@gmail.com>
2020-05-14 10:16:20 +0200

    reformatting

--- src/App.js
+++ src/App.js
@@ -19,25 +19,25 @@ import SideCart from './components/SideCart';
 import Footer from './components/Footer';
 
 class App extends Component {
-	render() {
-		return (
-			<>
-				<Navbar />
-				<Sidebar />
-				<SideCart />
-				<Switch>
-					<Route path="/" exact component={Home} />
-					<Route path="/about" component={About} />
-					<Route path="/contact" component={Contact} />
-					<Route path="/products" exact component={Products} />
-					<Route path="/products/:id" component={SingleProduct} />
-					<Route path="/cart" component={Cart} />
-					<Route component={Default} />
-				</Switch>
-				<Footer />
-			</>
-		);
-	}
+    render() {
+        return (
+            <>
+                <Navbar />
+                <Sidebar />
+                <SideCart />
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/about" component={About} />
+                    <Route path="/contact" component={Contact} />
+                    <Route path="/products" exact component={Products} />
+                    <Route path="/products/:id" component={SingleProduct} />
+                    <Route path="/cart" component={Cart} />
+                    <Route component={Default} />
+                </Switch>
+                <Footer />
+            </>
+        );
+    }
 }
 
 export default App;
