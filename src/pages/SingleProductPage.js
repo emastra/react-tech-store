d7197aed6001e0733879f65919be954d359d5d62
emastra <emiliano.mastra@gmail.com>
2020-05-14 10:12:46 +0200

    reformatting

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -7,65 +7,65 @@ import { CtxConsumer } from '../context';
 import { Link } from 'react-router-dom';
 
 export default function SingleProductPage() {
-	return (
-		<>
-			<Hero img={singleProductImg} title="single product" />
-			<CtxConsumer>
-				{(ctx) => {
-					const { singleProduct, addToCart, loading } = ctx;
+    return (
+        <>
+            <Hero img={singleProductImg} title="single product" />
+            <CtxConsumer>
+                {(ctx) => {
+                    const { singleProduct, addToCart, loading } = ctx;
 
-					if (loading) {
-						return <h1>product loading....</h1>;
-					}
+                    if (loading) {
+                        return <h1>product loading....</h1>;
+                    }
 
-					const { company, description, id, price, title, image } = singleProduct;
+                    const { company, description, id, price, title, image } = singleProduct;
 
-					return (
-						<section className="py-5">
-							<div className="container">
-								<div className="row">
-									<div className="col-10 mx-auto col-sm-8 col-md-6 my-3">
-										<img
-											src={`../${image}`}
-											// src={image}
-											alt="single product"
-											className="img-fluid"
-										/>
-									</div>
-									<div className="col-10 mx-auto col-sm-8 col-md-6 my-3">
-										<h5 className="text-title mb-4">model :{title} </h5>
-										<h5 className="text-capitalize text-muted mb-4">
-											company : {company}
-										</h5>
-										<h5 className="text-main text-capitalize mb-4">
-											price : ${price}
-										</h5>
-										<p className="text-capitalize text-title mt-3">
-											some info about product:
-										</p>
-										<p>{description}</p>
-										<button
-											type="button"
-											className="main-link"
-											style={{ margin: '0.75rem' }}
-											onClick={() => addToCart(id)}
-										>
-											add to cart
-										</button>
-										<Link
-											to="/products"
-											className="main-link"
-											style={{ margin: '0.75rem' }}
-										>
-											back to products
-										</Link>
-									</div>
-								</div>
-							</div>
-						</section>
-					);
-				}}
-			</CtxConsumer>
-		</>
-	);
+                    return (
+                        <section className="py-5">
+                            <div className="container">
+                                <div className="row">
+                                    <div className="col-10 mx-auto col-sm-8 col-md-6 my-3">
+                                        <img
+                                            src={`../${image}`}
+                                            // src={image}
+                                            alt="single product"
+                                            className="img-fluid"
+                                        />
+                                    </div>
+                                    <div className="col-10 mx-auto col-sm-8 col-md-6 my-3">
+                                        <h5 className="text-title mb-4">model :{title} </h5>
+                                        <h5 className="text-capitalize text-muted mb-4">
+                                            company : {company}
+                                        </h5>
+                                        <h5 className="text-main text-capitalize mb-4">
+                                            price : ${price}
+                                        </h5>
+                                        <p className="text-capitalize text-title mt-3">
+                                            some info about product:
+                                        </p>
+                                        <p>{description}</p>
+                                        <button
+                                            type="button"
+                                            className="main-link"
+                                            style={{ margin: '0.75rem' }}
+                                            onClick={() => addToCart(id)}
+                                        >
+                                            add to cart
+                                        </button>
+                                        <Link
+                                            to="/products"
+                                            className="main-link"
+                                            style={{ margin: '0.75rem' }}
+                                        >
+                                            back to products
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    );
+                }}
+            </CtxConsumer>
+        </>
+    );
 }
