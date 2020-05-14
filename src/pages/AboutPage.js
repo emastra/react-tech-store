7ae081057ea7efe18e4b8911b342b8d82a86bdce
emastra <emiliano.mastra@gmail.com>
2020-05-14 09:52:26 +0200

    reformatting

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -5,10 +5,10 @@ import Hero from '../components/Hero';
 import aboutBcg from '../images/aboutBcg.jpeg';
 
 export default function AboutPage() {
-	return (
-		<>
-			<Hero img={aboutBcg} />
-			<Info />
-		</>
-	);
+    return (
+        <>
+            <Hero img={aboutBcg} />
+            <Info />
+        </>
+    );
 }
